7901ab5eb2177d991cf8237aa3b7c4825caa55fd
Supra Dev <supra@devmail.com>
2025-11-30 19:19:39 +0530

    changes made to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+//default home page
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
-  const image="someimagepath"; // Replace with actual image path
+
   return (
    <div className="flex justify-center items-center flex-col">
     <h1  className=" font-pixelify-sans pt-6 text-center text-2xl">Welcome to the Home Page</h1>
+    <span> this page is the home page</span>
 
     <Button className="m-4 bg-primary font-inter">Click Me</Button>
    </div>
